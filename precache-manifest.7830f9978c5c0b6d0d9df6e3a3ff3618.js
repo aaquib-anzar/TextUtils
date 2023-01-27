be3aea37480d34a52fd614085ba02073577402be
aaquib-anzar <aaquib.anzar17@gmail.com>
2023-01-27 19:37:21 +0530

    Updates

---
+++ precache-manifest.7830f9978c5c0b6d0d9df6e3a3ff3618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5e5d7ffd4d23189e329",
+    "url": "/TextUtils/static/css/main.f72669a4.chunk.css"
+  },
+  {
+    "revision": "c5e5d7ffd4d23189e329",
+    "url": "/TextUtils/static/js/main.c5e5d7ff.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "31bd8d9406b83e5b62683a900cf3ab0d",
+    "url": "/TextUtils/index.html"
+  }
+];
